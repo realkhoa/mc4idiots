96c373b1576ef413eb676407511e53bcd0044c62
realkhoa <khoa.nguyenba@outlook.com>
2023-11-10 00:41:15 +0700

    remove MDX config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')()
 const nextConfig = {
   output: "export",
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
