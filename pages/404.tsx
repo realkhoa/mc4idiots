b23e90602b44c92763ff3a448596d662bb324512
realkhoa <khoa.nguyenba@outlook.com>
2023-11-10 00:54:12 +0700

    update 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Breadcrumb, Layout, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+import MainLayout from '../commons/layout/mainLayout';
+import styles from '../commons/styles/content.module.scss'
+
+export default function Home() {
+  return (
+    <MainLayout breadcrumbs={['404 Not Found']} theme='dark'>
+      <h2 className={styles.heading}>404</h2>
+      <p className={styles.paragraph}>
+        The content you requested doesn not exist.
+      </p>
+    </MainLayout>
+  )
+}
